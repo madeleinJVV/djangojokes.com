b7645df1ca28286363b1ac68844ba47ffb5575a6
madeleinJVV <madeleinjvvuuren@gmail.com>
2023-02-21 13:54:29 +0200

    joke model

--- jokes/models.py
+++ jokes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Joke(models.Model):
+    question = models.TextField(max_length=200)
+    answer = models.TextField(max_length=200, blank=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.question
+    
